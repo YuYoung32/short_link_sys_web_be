cc951a336add978da80f30eb6afc02f9ba64760f
YuYoung <yuyoung32@qq.com>
2023-05-02 17:15:42 +0800

    feat(/conf): use local file persistence
    [dev-version-0.3.1]
    - Use local file persistence instead of in config.yaml.

--- link_gen/simple_sequencer.go
+++ link_gen/simple_sequencer.go
@@ -6,32 +6,69 @@
 package link_gen
 
 import (
+	"os"
 	"short_link_sys_web_be/conf"
 	"short_link_sys_web_be/log"
+	"strconv"
 )
 
 var (
-	code         uint64 = 1000000000
-	startCodeKey        = "handler.link.simpleSeq.start"
+	code                               uint64 = 1000000000
+	startCodeKey                              = "handler.link.simpleSeq.start"
+	simpleSequencerPersistenceFilename        = "link_gen/simple_sequencer_persistence"
 )
 
 type SimpleSequencer struct{}
 
 func simpleSequencerInit() {
-	if conf.GlobalConfig.IsSet(startCodeKey) {
-		code = conf.GlobalConfig.GetUint64(startCodeKey)
-	} else {
-		log.GetLogger().Error("config file not set handler.link.simpleSeq.start, use default value 1000000000")
+	logger := log.GetLogger()
+	if conf.GlobalConfig.GetString("mode") == "dev" {
+		if conf.GlobalConfig.IsSet(startCodeKey) {
+			code = conf.GlobalConfig.GetUint64(startCodeKey)
+		} else {
+			logger.Error("config file not set handler.link.simpleSeq.start, use default value 1000000000")
+		}
+		return
 	}
+	filename := simpleSequencerPersistenceFilename
+
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		logger.Error("Failed to read file %s: %v", filename, err)
+	}
+	code, err = strconv.ParseUint(string(bytes), 10, 64)
+	logger.Infof("Read code %d from file %s", code, filename)
 }
 
 func simpleSequencerTerminate() {
-	conf.GlobalConfig.Set(startCodeKey, code)
-	err := conf.GlobalConfig.WriteConfig()
+	logger := log.GetLogger()
+	filename := simpleSequencerPersistenceFilename
+	err := os.WriteFile(filename, []byte(strconv.FormatUint(code, 10)), 0666)
 	if err != nil {
-		log.GetLogger().Errorf("write %v failed, value %v", startCodeKey, code)
+		logger.Error("Failed to write file %s: %v", filename, err)
 		return
 	}
+	//file, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC, 0666)
+	//if err != nil {
+	//	logger.Error("Failed to create file %s: %v", filename, err)
+	//}
+	//defer func(file *os.File) {
+	//	err := file.Close()
+	//	if err != nil {
+	//		logger.Error("Failed to close file %s: %v", filename, err)
+	//	}
+	//}(file)
+	//
+	//n, err := file.WriteString(strconv.FormatUint(code, 10))
+	//logger.Infof("Write %d bytes to file %s", n, filename)
+	//if n == 0 || err != nil {
+	//	logger.Error("Failed to write data to file")
+	//}
+	//err = file.Sync()
+	//if err != nil {
+	//	logger.Error("Failed to sync file %s: %v", filename, err)
+	//	return
+	//}
 }
 
 func (SimpleSequencer) GenLink(_ string) string {
